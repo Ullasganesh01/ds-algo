3c8faa4cb8f45ef5498f7328557487d8032f8ffb
Ullasganesh01 <139327604+Ullasganesh01@users.noreply.github.com>
2023-09-22 21:13:23 +0530

    Update CountOfRepeatedEleArray.java

--- basic_java_prog/CountOfRepeatedEleArray.java
+++ basic_java_prog/CountOfRepeatedEleArray.java
@@ -17,6 +17,24 @@ public class CountOfRepeatedEleArray {
 		}
 		System.out.println(" "+ a[a.length-1]+"   "+count);
     }
+    public static void countOfRepeatedElementsGreaterThanOne(int a[]){
+        int count=1;
+        System.out.println("Ele  Count : ");
+		for(int i=0;i<a.length-1;i++) {
+			if(a[i]==a[i+1]) {
+				count++;
+			}
+			else {
+                if(count>1){
+                    System.out.println(" "+a[i]+"   "+count);
+                }
+				count=1;
+			}
+		}
+        if(count>1){
+            System.out.println(" "+ a[a.length-1]+"   "+count);
+        }	
+    }
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.println("Enter the size : ");
@@ -27,6 +45,7 @@ public class CountOfRepeatedEleArray {
             arr[i] = s.nextInt();
         }
         countOfRepeatedElements(arr);
+	countOfRepeatedElementsGreaterThanOne(arr);
         s.close();
     }
 }
